e7db2e022ceeed358c24c8ba1ddaadbdc00ff098
leo <394325498@qq.com>
2020-05-22 11:05:00 +0800

    底层数据中间价性能测试编写并调优

---
+++ db/database/databaseimpl_test.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"gitee.com/bidpoc/database-fabric-cc/db"
+	"github.com/golang/protobuf/proto"
+	"testing"
+)
+
+func TestRow(t *testing.T) {
+	columnSize := 1024*1024
+	columnData := make([]byte, 0, columnSize)
+	for i:=0;i<columnSize;i++{
+		columnData = append(columnData,1)
+	}
+	size := 5000
+	columns := make([]*ColumnData, 0, size)
+	for i:=0;i<size;i++ {
+		columns = append(columns, &ColumnData{Data:columnData})
+	}
+	row := &RowData{Id:db.RowID(1),Op:uint32(db.ADD),Columns:columns}
+	proto.Marshal(row)
+	//fmt.Println("序列化之后的信息为：",buffer)
+	//newRow := &RowData{}
+	//proto.Unmarshal(buffer, newRow)
+	//fmt.Println("反序列化之后的信息为：",newRow)
+}
